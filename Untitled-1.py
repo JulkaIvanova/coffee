a67188bb273e25e65c522f29ac492cfcc8ed8187
JulkaIvanova <yulchisha2009@gmail.com>
2024-12-02 14:51:23 +0500

    h

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication,  QMainWindow, QTableWidgetItem, QTableWidget
+from PyQt6.QtWidgets import QApplication,  QMainWindow, QTableWidgetItem, QTableWidget, QWidget
 from PyQt6.QtGui import QPainter, QColor, QPen
 from PyQt6 import uic
 import random
@@ -7,12 +7,43 @@ import sqlite3
 from PyQt6.QtCore import Qt
 
 
+class Add(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("untitled2.ui", self)
+        self.pushButton.clicked.connect(self.add)
+    
+    def add(self):
+        self.connection = sqlite3.connect("coffee.db")
+        self.connection.cursor().execute(f"INSERT INTO coffee(name, roasting, ground_in_grains, taste, price, volume) VALUES({self.lineEdit_2.text()}, {self.lineEdit_3.text()}, {self.lineEdit_4.text()}, {self.lineEdit_5.text()}, {self.lineEdit_6.text()}, {self.lineEdit_7.text()})")
+        self.close()
+
+class Update(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("untitled1.ui", self)
+        self.pushButton.clicked.connect(self.update)
+    
+    def update(self):
+        self.connection = sqlite3.connect("coffee.db")
+        self.connection.cursor().execute(f"UPDATE coffee SET name = {self.lineEdit_2.text()}, roasting = {self.lineEdit_3.text()}, ground_in_grains = {self.lineEdit_4.text()}, taste = {self.lineEdit_5.text()}, price = {self.lineEdit_6.text()}, volume = {self.lineEdit_7.text()} WHERE ID = {self.lineEdit.text()}")
+        self.close()
+
 class MyNotes(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('untitled.ui', self)
         self.pushButton.clicked.connect(self.select_data)
-        
+        self.action_2.triggered.connect(self.addAct)
+        self.action.triggered.connect(self.updateAct)
+    
+    def updateAct(self):
+        self.windowadd = Update()
+        self.windowadd.show()
+
+    def addAct(self):
+        self.windowadd = Add()
+        self.windowadd.show()
 
     def select_data(self):
         self.connection = sqlite3.connect("coffee.db")
